783d77ab431927746f446a1b0b00814bbece2270
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-21 20:05:05 +0300

    updated

--- Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
@@ -1,6 +1,8 @@
 const regionSearchBar = document.querySelector('#region')
 const countryDropdown = document.querySelector('#country')
+const factsArea = document.querySelector('#facts-area')
 const regionBaseUrl = 'https://restcountries.com/v3.1/region'
+const countryBaseUrl = 'https://restcountries.com/v3.1/name'
 
 async function handleRegionChanged(region) {
     const selectedRegion = regionSearchBar.value
@@ -11,13 +13,46 @@ async function handleRegionChanged(region) {
 
     const options = countries.map(country => `<option value="${country.name.common}">${country.name.common}</option>`)
     countryDropdown.innerHTML = options.join(' ')
-
+    await handleCountryChanged(countryDropdown.value)
 
 }
 
-const url = `${regionBaseUrl}/${selectedRegion}`
-console.log(url)
+async function handleCountryChanged(selectedCountry) {
+    const url = `${countryBaseUrl}/${selectedCountry}`
+    //download the data
+    const data = await fetch(url)
+    const countries = await data.json()
+    factsArea.innerHTML = factsHTML(countries[0])
 
+}
+
+function factsHTML(country) {
+    const key = Object.keys(country.currencies)[0]
+    console.log(key)
+
+    return `
+        <h1>Fact about ${country.name.common}</h1>
+        <img src="${country.flags.png}">
+        <table>
+            <tr>
+                <td>Offical Country Name</td>
+                <td>${country.name.common}</td>
+            </tr> 
+            <tr>
+                <td>Capital City</td>
+                <td>${country.capital}</td>
+            </tr>
+             <tr>
+                <td>Population</td>
+                <td>${country.population}</td>
+            </tr>
+               <tr>
+                <td>Currency</td>
+                <td>${country.currencies[key].name}</td>
+            </tr>
+        </table>
+    `
+}
 
 
 
