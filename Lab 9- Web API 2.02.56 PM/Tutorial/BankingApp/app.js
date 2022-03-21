8c76530da4376365a4ca72230166d219e7bd2621
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-21 19:12:54 +0300

    Update app.js

--- Lab 9- Web API 2.02.56 PM/Tutorial/BankingApp/app.js
+++ Lab 9- Web API 2.02.56 PM/Tutorial/BankingApp/app.js
@@ -21,15 +21,16 @@ let accounts = [
 ]
 
 app.use(express.static('public'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.download('package.json')
 })
 
-app.get('/accounts', (req, res) => {
+app.get('/api/accounts', (req, res) => {
     res.send(accounts)
 })
-app.delete('/accounts/:accountNo', (req, res) => {
+app.delete('/api/accounts/:accountNo', (req, res) => {
     const accountsNo = req.params.accountNo
     const totalAccounts = accounts.length
     accounts = accounts.filter(account => account.accountNo != accountsNo)
@@ -39,14 +40,23 @@ app.delete('/accounts/:accountNo', (req, res) => {
     else
         res.send(`you successfully deleted ${accountsNo}`)
 })
-
-app.put('/accounts', (req, res) => {
-    res.send('you are updating')
+app.post('/api/accounts', (req, res) => {
+    const account = req.body
+    accounts.push(account)
+    res.send(`successfully added the account ${account.accounNo}`)
 })
 
-app.post('/accounts', (req, res) => {
-    res.send('you are adding this')
+app.put('/api/accounts', (req, res) => {
+    const updatedAccount = req.body
+    const index = accounts.findIndex(account => account.accountNo == updatedAccount.accountNo)
+    if(index < 0 )
+        res.send(`unable to update account`)
+    else  {
+        accounts[index] = updatedAccount
+        res.send(`successfully updated the account ${updatedAccount.accountNo}`)
+    }
 })
+
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`)
