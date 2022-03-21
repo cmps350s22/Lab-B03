bd3280e61e7324004a5464a22f2717aab4f31aba
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-21 18:33:33 +0300

    Update app.js

--- Lab 9- Web API 2.02.56 PM/Tutorial/BankingApp/app.js
+++ Lab 9- Web API 2.02.56 PM/Tutorial/BankingApp/app.js
@@ -1,16 +1,22 @@
 import express from 'express'
+
 const app = express()
 const port = 3000
 let accounts = [
     {
-        name : "Mohamed",
-        accountNo : 123,
-        balance : 300
+        name: "Mohamed",
+        accountNo: 123,
+        balance: 300
     },
     {
-        name : "Ahmed",
-        accountNo : 5555,
-        balance : 6666
+        name: "Ahmed",
+        accountNo: 444,
+        balance: 6666
+    },
+    {
+        name: "Ali",
+        accountNo: 124,
+        balance: 6666
     }
 ]
 
@@ -20,24 +26,29 @@ app.get('/', (req, res) => {
     res.download('package.json')
 })
 
-app.get('/accounts', (req, res)=>{
+app.get('/accounts', (req, res) => {
     res.send(accounts)
 })
-app.delete('/accounts/:accountNo', (req, res)=>{
+app.delete('/accounts/:accountNo', (req, res) => {
     const accountsNo = req.params.accountNo
+    const totalAccounts = accounts.length
+    accounts = accounts.filter(account => account.accountNo != accountsNo)
 
-    res.send(`you are deleting ${accountsNo}`)
+    if (totalAccounts == accounts.length)
+        res.send(`account no ${accountsNo} does not exist`)
+    else
+        res.send(`you successfully deleted ${accountsNo}`)
 })
 
-app.put('/accounts', (req, res)=>{
+app.put('/accounts', (req, res) => {
     res.send('you are updating')
 })
 
-app.post('/accounts', (req, res)=>{
+app.post('/accounts', (req, res) => {
     res.send('you are adding this')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`)
 })
 
