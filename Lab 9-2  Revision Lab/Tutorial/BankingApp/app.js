f6d76af9e2378b9e5752d699c3b2f6c34087b7c2
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-28 17:30:10 +0300

    app.js routes

--- Lab 9-2  Revision Lab/Tutorial/BankingApp/app.js
+++ Lab 9-2  Revision Lab/Tutorial/BankingApp/app.js
@@ -12,14 +12,36 @@
     4. if you want to assign a port in your system
     ->window SET PORT = 3000
     ->mac EXPORT PORT = 3000
+    5. to automatically restart your sever
+    => nodemon
  */
 import express from 'express';
+
 const app = express()
 
 //every app run on specific port number
 //Godaddy
 const port = process.env.PORT || 4000
 
-app.listen(port, ()=>{
+//first route
+app.get('/api/accounts', getAccounts)
+app.post('/api/accounts', addAccount)
+app.delete('/api/accounts', deleteAccount)
+app.put('/api/accounts', updateAccount)
+
+function getAccounts(req, res) {
+    res.send('Welcome to get accounts route')
+}
+function addAccount(req, res) {
+    res.send('Welcome to add a single account route')
+}
+function deleteAccount(req, res) {
+    res.send('Welcome to delete a single account route')
+}
+function updateAccount(req, res) {
+    res.send('Welcome to update a single account route')
+}
+
+app.listen(port, () => {
     console.log(`server listening on http://localhost:${port}`)
 })
